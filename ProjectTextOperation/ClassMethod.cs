b13323f04514562a4bfd003d1d500b61d8ab086c
Alex Shvedov <User@DESKTOP-V2MV0KM>
2019-09-30 19:28:32 +0300

    text commit

---
+++ ProjectTextOperation/ClassMethod.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ProjectTextOperation
+{
+    class ClassMethod
+    {
+        static public string textRead(string fileLocation) {
+            string text = null;
+            using (StreamReader reader = new StreamReader(fileLocation, Encoding.Default))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+        static public List<Tuple<string, int>> CountWord(string [] massText) {
+            List<Tuple<string, int>> lst = new List<Tuple<string, int>>();
+            for (int i = 0; i < massText.Length; i++)
+            {
+                int count = 0;
+                Tuple<string, int> tupl;
+                for (int j = 0; j < massText.Length; j++)
+                {
+                    if (massText[i] == massText[j])
+                    {
+                        count++;
+                    }
+                }
+                tupl = Tuple.Create(massText[i], count);
+                lst.Add(tupl);
+            }
+            return lst;
+        }
+
+        static public List<Tuple<string, int>> BubbleSort( List<Tuple<string, int>> lst) {
+            for (int i = 0; i < lst.Count; i++)
+            {
+                for (int j = 0; j < lst.Count; j++)
+                {
+                    if (lst[i].Item2 < lst[j].Item2)
+                    {
+                        Tuple<string, int> temp;
+                        temp = lst[i];
+                        lst[i] = lst[j];
+                        lst[j] = temp;
+                    }
+                }
+            }
+            return lst;
+        }
+
+        public static void textWrite(List<Tuple<string, int>> lst, string fileLocation)
+        { 
+              using(StreamWriter strW = new StreamWriter(fileLocation, false, Encoding.Default)){
+                  foreach (Tuple<string, int> p in lst) strW.WriteLine("Слово {0} |количесвто повторений {1} ", p.Item1, p.Item2);
+              }
+        }
+    }
+}
---
+++ ProjectTextOperation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ProjectTextOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\programmC#\TextPoem.txt";
+            string pathW = @"D:\programmC#\TextC#.txt";
+            string text = null;
+            text = ClassMethod.textRead(path);
+            List<Tuple<string, int>> lst = new List<Tuple<string, int>>();
+            string[] massText = text.Split(new char[]{' ', ',', '.','?', '-', '!',':','\n','\r'}, StringSplitOptions.RemoveEmptyEntries);    
+            lst = ClassMethod.CountWord(massText);
+            lst = lst.Distinct().ToList();    
+            lst = ClassMethod.BubbleSort(lst);
+            foreach (Tuple<string, int> p in lst) Console.WriteLine("Слово {0} |количесвто повторений {1} ", p.Item1, p.Item2);
+            ClassMethod.textWrite(lst, pathW);
+        }
+ 
+
+    }
+}
